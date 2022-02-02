aedeb5052ebb697681930fea746f4c798200762f
ro-nin <ema.cio1793@gmail.com>
2022-02-02 17:22:09 +0100

    button style gradient

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -9,7 +9,12 @@ const Landing = (props: Props) => {
 
         <p className='mt-8 text-2xl font-bold text-white shadow-lg uppercase'>A Disco Elysium UNOFFICIAL character spreadsheet generator.</p>
         <p className=' text-xl font-bold  text-gray-400 uppercase'>An high concept tool for high concept people (Desktop Only). </p>
-        <a href="/#stats" className=' mt-6 scroll-smooth text-2xl rounded my-2 font-bold text-white bg-black p-4 uppercase hover:bg-opacity-80'> Start</a>
+        <a href="/#stats">
+            <div className=' mt-6 scroll-smooth text-3xl rounded my-2  font-extrabold  bg-black p-4 uppercase hover:bg-opacity-80'>
+                <p className=' font-bold bgLandingGradientText  uppercase hover:bg-opacity-80'> Start</p>
+            </div>
+        </a>
+
 
         <p className=' mt-36'>All text content and artworks shown in this site are registered property of studio ZAUM, I do not own any rights.</p>
 
